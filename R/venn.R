cd0e3a9a3f3b01835f31d65167a1d360206b9ecd
ymat2 <ymat2@users.noreply.github.com>
2024-11-20 19:32:39 +0900

    :pen: Summarise venn diagram in R

--- R/venn.R
+++ R/venn.R
@@ -1,39 +1,102 @@
+library(conflicted)
+library(tidyverse)
+
+
+setA = stringr::str_c("gene", as.character(sample(100, 45)), sep = "_")
+setB = stringr::str_c("gene", as.character(sample(100, 55)), sep = "_")
+
+
+##### 1 ##### VennDiagram
+
 library(VennDiagram)
 
-# サンプルデータ
-set.seed(20190708)
-genes <- paste("gene",1:1000,sep="")
-x <- list(
-  A = sample(genes,300), 
-  B = sample(genes,525), 
-  C = sample(genes,440),
-  D = sample(genes,350)
+data = list(setA = setA, setB = setB)
+vd1 = VennDiagram::venn.diagram(
+  data, 
+  filename = NULL, 
+  disable.logging = TRUE, 
+  height =3000, 
+  width = 3000, 
+  resolution = 500, 
+  # imagetype = "tiff", 
+  units = "px", 
+  na = "stop", 
+  main = "Here is the title", 
+  sub = "Here is subtitle", 
+  main.pos = c(0.5, 1.05), 
+  main.fontface = "plain",
+  main.fontfamily = "serif", 
+  main.col = "black",
+  main.cex = 1, 
+  main.just = c(0.5, 1), 
+  sub.pos = c(0.5, 1.05), 
+  sub.fontface = "plain", 
+  sub.fontfamily = "serif", 
+  sub.col = "black", 
+  sub.cex = 1, 
+  sub.just = c(0.5, 1), 
+  category.names = c("Gene set A", "Gene set B"), 
 )
 
-# ベン図を描画
-venn.plot <- VennDiagram::venn.diagram(
-  x,
-  category.names = c("Group 1", "Group 2"),
-  filename = NULL
-)
+grid.draw(vd1)
 
-# 描画
-grid.draw(venn.plot)
 
+##### 2 ##### ggvenn
 
 library(ggvenn)
 
-ggvenn::ggvenn(
-  data = x,
-  columns = c("A", "B"),
+data = list(setA = setA, setB = setB)
+vd2 = ggvenn::ggvenn(
+  data,
+  columns = c("setA", "setB"),
   show_elements = FALSE,
   show_percentage = FALSE,
-  stroke_size = 0.5,
+  digits = 1,
+  fill_color = c("blue", "yellow", "green", "red"),
+  fill_alpha = 0.5,
+  stroke_color = "black",
+  stroke_alpha = 1,
+  stroke_size = 1,
   stroke_linetype = "solid",
-  # fill_color = c("darkorange", "blue"),
-  set_name_color = "red",
-  set_name_size = 5,
-  text_color = "#444444",
-  text_size = 8,
-  auto_scale = FALSE
-) + scale_fill_viridis_d()
+  set_name_color = "black",
+  set_name_size = 6,
+  text_color = "black",
+  text_size = 4,
+  label_sep = ",",
+  count_column = NULL,
+  show_outside = "none",
+  auto_scale = TRUE
+)
+
+vd2
+
+
+##### 3 ##### ggVennDiagram
+
+library("ggVennDiagram")
+
+data = list(setA = setA, setB = setB)
+vd3 = ggVennDiagram::ggVennDiagram(
+  data,
+  category.names = c("Gene set A", "Gene set B"),
+  show_intersect = FALSE,
+  set_color = "black",
+  set_size = NA,
+  label = "count",
+  label_alpha = 0.5,
+  label_geom = "label",
+  label_color = "black",
+  label_size = NA,
+  label_percent_digit = 0,
+  label_txtWidth = 40,
+  edge_lty = "solid",
+  edge_size = 1,
+  force_upset = FALSE,
+  nintersects = 20,
+  order.intersect.by = "size",
+  order.set.by = "size",
+  relative_height = 3,
+  relative_width = 0.3
+) + scale_fill_viridis_c()
+
+vd3
