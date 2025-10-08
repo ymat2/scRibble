f8d6426647c43c027e578f87ea8ccdf93ba9d0c6
ymat2 <ymat2@users.noreply.github.com>
2025-10-08 10:34:16 +0900

    :sparkles: Implement exponent label

--- R/label_scale.R
+++ R/label_scale.R
@@ -11,20 +11,21 @@ default
 
 default + scale_y_continuous(labels = scales::label_number())
 default + scale_y_continuous(labels = scales::label_comma())
+default + scale_y_continuous(labels = scales::label_log())
 default + scale_y_continuous(labels = scales::label_number(scale = 1/1000, suffix = "k"))
 default + scale_y_continuous(labels = scales::label_number(scale_cut = scales::cut_long_scale()))
 
 default + scale_y_continuous(labels = scales::trans_format('log10', scales::math_format(10^.x)))
 
 
-scientific_notation <- function(x) {
-  x = format(x, scientific = TRUE)
-  x = gsub("^(.*)e", "'\\1'e", x)
-  x = gsub("e", "%*%10^", x)
-  x = gsub('\\+', '', x)
-  parse(text = x)
+label_exp = function() {
+  function(x) {
+    x = format(x, scientific = TRUE)
+    x = gsub("^(.*)e", "'\\1'e", x)
+    x = gsub("e", "%*%10^", x)
+    x = gsub('\\+', '', x)
+    ggplot2:::parse_safe(text = x)
+  }
 }
 
-label_sci = function() scientific_notation
-
-default + scale_y_continuous(labels = label_sci())
+default + scale_y_continuous(labels = label_exp())
